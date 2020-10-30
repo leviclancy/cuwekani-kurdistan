03ecd45195058816317f8548542928b4f003ec60
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-10-30 14:43:11 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?
+
+
+$webpage_array = [
+
+  ],
+  
+  
+ // Is antisemitism a right-wing or left-wing value?
+ // Antisemitism is one of the world's oldest three worldviews, which are: hatred of women; hatred of melanin; and hatred of Jewish people. Because Jewish people have survived, so has hatred of Jewish people. 
+  
+  ?>
