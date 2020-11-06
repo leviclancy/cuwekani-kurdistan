8ec76bfa62faeb2ef20ce2700bacf4dc5b3bd857
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-11-06 13:23:58 +0300

    Update translatable-elements.php

--- translatable-elements.php
+++ translatable-elements.php
@@ -6,30 +6,6 @@
 		"ar" => "",
 		],
 
-	"is-zionism-supremacism" => [
-		"en" => "",
-		"ku" => "",
-		"ar" => "",
-		],
-
-	"is-antizionism-antisemitism" => [
-		"en" => "",
-		"ku" => "",
-		"ar" => "",
-		],
-
-	"spelling-jews" => [
-		"en" => "The Arabic work 'yahud' is the basis for the Kurmanci word 'cihud' which, over time, has lost the 'd' and is now generally rendered as 'cihu'. In the Sorani, this was further simplified into just 'cu' which also mirrors the English (and by default, international) term. The term 'culeke' uses the Arabic diminutive, and although this has also entered common circulation and is often meant neutrally, it still carries that negative connotation and is not the preferred term.",
-		"ku" => "",
-		"ar" => "",
-		],
-
-	"spelling-the-national-association" => [
-		"en" => "It can be shorted as just 'National Association' in English. In Hebrew, it is shorted as just 'Hargun' usually but this is not to be confused with the armed forces of the same (generic) name.",
-		"ku" => "",
-		"ar" => "",
-		],
-
 	"" => [
 		"en" => "",
 		"ku" => "",
