c834ce91a13c6022781cef8faf4a62e932414666
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-11-15 04:05:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
   ],
 
+// Placeholder
 
+echo "<a href='https://instagram.com/cuwekani_kurdistan'>instagram.com/cuwekani_kurdistan</a>";
 
-  ?>
+echo "<a href='https://facebook.com/cuwekani-kurdistan'>facebook.com/cuwekani-kurdistan</a>"; ?>
