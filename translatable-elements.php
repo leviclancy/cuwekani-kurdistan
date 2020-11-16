18768a5c2a273836952ca04e1085e0303a0df345
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-11-16 03:39:13 +0300

    Update translatable-elements.php

--- translatable-elements.php
+++ translatable-elements.php
@@ -1,8 +1,428 @@
 <? $translatable_elements = [
 
-	"the-jews-of-kurdistan" => [
-		"en" => "The Jews of Kurdistan",
+	"homepage" => [
+		"en" => "Homepage",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"jews-from-kurdistan" => [
+		"en" => "Jews from Kurdistan",
 		"ku" => "جووەکانی کوردستان"
+		"ar" => "",
+		],
+
+	"spiritual-matters" => [
+		"en" => "Spiritual matters",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"econonomic-matters" => [
+		"en" => "Economic matters",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"judeo-aramaic" => [
+		"en" => "Judeo-Aramaic language",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"community-relations" => [
+		"en" => "Community relations",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"relations-with-kurds" => [
+		"en" => "Relations with Kurds",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"relations-with-assyrians" => [
+		"en" => "Relations with Assyrians",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"relations-with-ezidis" => [
+		"en" => "Relations with Ezidis",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"relations-with-turkomans" => [
+		"en" => "Relations with Turkomans",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"relations-with-governments" => [
+		"en" => "Relations with governments",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"relations-with-other-jews" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"community-institutions" => [
+		"en" => "Community institutions",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"community-leaders" => [
+		"en" => "Community leaders",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"expulsions-of-jews" => [
+		"en" => "Expulsions of Jews",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"operation-ezra-and-nehemiah" => [
+		"en" => "Operation Ezra and Nehemiah",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"what-is-aliyah" => [
+		"en" => "What is aliyah?",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"communities" => [
+		"en" => "Communities",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"urmia-rizaiyeh" => [
+		"en" => "Urmia-Rizaiyeh (אורמיה-ריזאיה)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"urfa" => [
+		"en" => "Urfa (אורפה)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"erbil" => [
+		"en" => "Erbil (ארביל)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"betanure" => [
+		"en" => "Betanure (ביתנורי)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"barashe" => [
+		"en" => "Barashe (בראשי)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"barzan" => [
+		"en" => "Barzan (ברזאן)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"duhok" => [
+		"en" => "Duhok (דוהוכ)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"zakho" => [
+		"en" => "Zakho (זאכו)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"kirkuk" => [
+		"en" => "Kirkuk (כרכוכ)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"nerwa" => [
+		"en" => "Nerwa (נירווא)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"sindur" => [
+		"en" => "Sindur (סונדור)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"sulaymaniyah" => [
+		"en" => "Sulaymaniyah (סלימניה)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"amediya" => [
+		"en" => "Amediya (עמדיה)",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
+		"ar" => "",
+		],
+
+	"" => [
+		"en" => "",
+		"ku" => "",
 		"ar" => "",
 		],
 
