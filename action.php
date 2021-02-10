ed9d4c203f65569fef25650734884577d561053d
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-10 14:31:21 -0800

    Update action.php

--- action.php
+++ action.php
@@ -1,9 +1,23 @@
 <?
 
+
+$source = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQeIaZ0kiGqTr5qNGctkfdT980boNxLa9NXofw-yhEC8PoVPlgKKBD4Uyfdba6Qd9uJfh44bugoNN85/pub?output=tsv";
+
+// Periodically capture and save it as a TXT file
+
+// See if it's been recently updated. If not, then recapture it.
+
 // Pull all content, make a word cloud
 
+
+// Overall view
+  // Language toggle
+  // Includes search
+
+// Single view — ID & language in URL
 // 100% in FAQ format — fact-checing format
 
 // Each one is highly sharable and also has a meme format
+
 
 ?>
