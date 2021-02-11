f194a747cea3a70a5ec607be5bf2e2eefbfa7941
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-11 10:47:03 -0800

    Update action.php

--- action.php
+++ action.php
@@ -2,7 +2,12 @@
 
 $source = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQeIaZ0kiGqTr5qNGctkfdT980boNxLa9NXofw-yhEC8PoVPlgKKBD4Uyfdba6Qd9uJfh44bugoNN85/pub?output=tsv";
 
+// Get file contexts of the source.json
+
 // Periodically capture and save it as a TXT file
+	// Get it from Google Docs
+	// Convert to array
+	// Save as JSON in a .json file
 
 // See if it's been recently updated. If not, then recapture it.
 
